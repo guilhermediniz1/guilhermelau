e1a65f3eecb3a86588d6fe09a2a6ad5416a0af8f
Guilherme Láu <guilhermedinizlau@gmail.com>
2026-02-24 20:26:33 -0300

    adiciona nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,18 @@ export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
+
+  app: {
+    baseURL: '/guilhermelau/',
+  },
+
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ['/'],
+    },
+  },
+
   googleFonts: {
     families: {
       'Barlow': [400, 700, 900],
